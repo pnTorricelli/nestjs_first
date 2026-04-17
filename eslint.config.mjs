7d77f49f6e8c4b8411b712e3a611ffe838b607d5
Nicolò Pincelli <pincelli.n@gmail.com>
2026-04-17 20:18:35 +0200

    refactor: update task model and service to use UUID for task IDs, add createTask method in controller

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,25 @@
-// @ts-check
+// eslint.config.mjs
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import globals from 'globals';
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended, // 👈 NON typeChecked
   {
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 );
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import type { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) { }
 
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  };
+
+  @Post()
+  createTask(@Body() body: { title: string; description: string }): Task {
+    return this.tasksService.createTask(body.title, body.description);
   }
 }
